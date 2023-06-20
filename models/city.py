f453f7d17568a213a83624c826e1532d99e5493b
OmobaVII <omoba.career@gmail.com>
2023-06-20 07:35:19 -0700

    DbStorage - Place

--- models/city.py
+++ models/city.py
@@ -4,6 +4,7 @@ from models.base_model import BaseModel, Base
 from os import getenv
 from models.state import State
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
@@ -12,6 +13,8 @@ class City(BaseModel, Base):
     if getenv("HBNB_TYPE_STORAGE") == "db":
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+        places = relationship("Place", backref="cities",
+                              cascade="all, delete")
     else:
         state_id = ""
         name = ""
