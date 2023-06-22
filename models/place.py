0626166b81110a6a1c22f30f395d158cf2c1910e
OmobaVII <omoba.career@gmail.com>
2023-06-22 09:26:54 -0700

    added some update of city, user, state and place

--- models/place.py
+++ models/place.py
@@ -17,22 +17,20 @@ class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = "places"
 
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
-        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
-        name = Column(String(128), nullable=False)
-        description = Column(String(1024), nullable=True)
-        number_rooms = Column(Integer, default=0, nullable=False)
-        number_bathrooms = Column(Integer, default=0, nullable=False)
-        max_guest = Column(Integer, default=0, nullable=False)
-        price_by_night = Column(Integer, default=0, nullable=False)
-        latitude = Column(Float, nullable=True)
-        longitude = Column(Float, nullable=True)
-        review = relationship("Review", backref="place", cascade="all,\
-                              delete, delete-orphan")
-        amenities = relationship("Amenity", secondary=place_amenity, viewonly=False)
+    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=True)
+    number_rooms = Column(Integer, default=0, nullable=False)
+    number_bathrooms = Column(Integer, default=0, nullable=False)
+    max_guest = Column(Integer, default=0, nullable=False)
+    price_by_night = Column(Integer, default=0, nullable=False)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
+    review = relationship("Review", backref="place", cascade="delete")
+    amenities = relationship("Amenity", secondary=place_amenity, viewonly=False)
 
-    else:
+    if getenv("HBNB_TYPE_STORAGE") != "db":
         city_id = ""
         user_id = ""
         name = ""
--- models/state.py
+++ models/state.py
@@ -10,11 +10,10 @@ from sqlalchemy.orm import relationship
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state",
-                              cascade="all, delete")
-    else:
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="delete")
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
         name = ""
 
         @property
@@ -22,7 +21,7 @@ class State(BaseModel, Base):
             """provides the filestorage relationship between
             cities and states"""
             city_list = []
-            for city in models.storage.all("City").values():
+            for city in models.storage.all(City).values():
                 if city.state_id == State.id:
                     city_list.append(city)
             return city_list
