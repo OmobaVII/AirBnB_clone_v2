3d8ae74eece9c0e9ebce6427673a1fa135055f3b
root <root@O-SANNI.Edostate.gov.ng>
2023-07-05 19:50:06 +0100

    Prepare your web servers

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# Install Nginx if it not already installed
+# Create the folder /data/ if it doesn’t already exist
+# Create the folder /data/web_static/ if it doesn’t already exist
+# Create the folder /data/web_static/releases/ if it doesn’t already exist
+# Create the folder /data/web_static/shared/ if it doesn’t already exist
+# Create the folder /data/web_static/releases/test/ if it doesn’t already exist
+# Create a fake HTML file /data/web_static/releases/test/index.html
+# Create a symbolic link /data/web_static/current linked to the /data/web_static/releases/test/ folder
+# Give ownership of the /data/ folder to the ubuntu user AND group
+# Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static
+
+content="\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
+
+sudo apt-get -y update
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/releases/test
+sudo mkdir -p /data/web_static/shared
+sudo echo "Test for my nginx configuration" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -hR ubuntu:ubuntu /data/
+sudo sed -i '35a\'"$content" /etc/nginx/sites-available/default
+sudo /etc/init.d/nginx restart
