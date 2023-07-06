5c3ff4625c7859cb381a4fe13ec57e3f4e0e2d91
OmobaVII <omoba.career@gmail.com>
2023-07-06 13:00:15 -0700

    Try

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -15,23 +15,18 @@ def do_deploy(archive_path):
     """deploy web_static using fabric"""
     if not exists(archive_path):
         return False
-    filename_ext = archive_path.split("/")[-1]
-    filename = filename_ext.split(".")[0]
-    full_path = "/data/web_static/releases/{}".format(filename)
-    if put(archive_path, "/tmp/").failed:
+    try:
+        put(archive_path, "/tmp/")
+        filename_ext = archive_path.split("/")[-1]
+        filename = filename_ext.split(".")[0]
+        full_path = "/data/web_static/releases/{}".format(filename)
+        run("sudo mkdir -p {}".format(full_path))
+        run("sudo tar -xzf /tmp/{} -C {}/".format(filename_ext, full_path))
+        run("sudo rm /tmp/{}".format(filename_ext))
+        run("sudo mv {}/web_static/* {}/".format(full_path, full_path))
+        run("sudo rm -rf {}/web_static".format(full_path))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s {} /data/web_static/current".format(full_path))
+        return True
+    except BaseException:
         return False
-    if run("mkdir -p {}".format(full_path)).failed:
-        return False
-    if run("tar -xzf /tmp/{} -C {}/".format(filename_ext, full_path)).failed:
-        return False
-    if run("rm /tmp/{}".format(filename_ext)).failed:
-        return False
-    if run("mv {}/web_static/* {}/".format(full_path, full_path)).failed:
-        return False
-    if run("rm -rf {}/web_static".format(full_path)).failed:
-        return False
-    if run("rm -rf /data/web_static/current").failed:
-        return False
-    if run("ln -s {} /data/web_static/current".format(full_path)).failed:
-        return False
-    return True
