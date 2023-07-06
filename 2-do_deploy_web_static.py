06b5b371e2f6dc47e8fd0c38084c6097a0f3e0fa
OmobaVII <omoba.career@gmail.com>
2023-07-06 10:08:24 +0000

    Deploy web_static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,7 +9,7 @@ from os.path import exists
 
 
 env.user = "ubuntu"
-env.hosts = "['18.235.234.111', '100.25.181.230']"
+env.hosts = ['18.235.234.111', '100.25.181.230']
 
 
 def do_deploy(archive_path):
@@ -19,12 +19,12 @@ def do_deploy(archive_path):
     try:
         put(archive_path, "/tmp/")
         filename_ext = archive_path.split("/")[-1]
-        filename = filename.split(".")[0]
+        filename = filename_ext.split(".")[0]
         full_path = "/data/web_static/releases/{}".format(filename)
-        run("sudo mkdir -p /data/web_static/releases/{}/".format(filename))
+        run("sudo mkdir -p {}".format(full_path))
         run("sudo tar -xzf /tmp/{} -C {}/".format(filename_ext, full_path))
         run("sudo rm /tmp/{}".format(filename_ext))
-        run("sudo mv {0}/web_static/* {0}/".format(full_path))
+        run("sudo mv {}/web_static/* {}/".format(full_path, full_path))
         run("sudo rm -rf {}/web_static".format(full_path))
         run("sudo rm -rf /data/web_static/current")
         run("sudo ln -s {}/ /data/web_static/current".format(full_path))
