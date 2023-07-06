f378a7a56d49b0cc30336d559bcad3f9e4c8d496
OmobaVII <omoba.career@gmail.com>
2023-07-06 16:10:31 -0700

    Puppet for setup

---
+++ 101-setup_web_static.pp
@@ -0,0 +1,40 @@
+# Web server setup and configuration
+
+exec { 'add nginx stable repo':
+    command => 'sudo add-apt-repository ppa:nginx/stable',
+    path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
+exec { 'update packages':
+    command => 'apt-get -y update',
+    path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
+package { 'nginx':
+    ensure => 'installed',
+}
+exec { 'mkdir':
+    command => 'sudo mkdir -p /data/web_static/releases/test',
+    path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
+exec { 'mkdir again':
+    command => 'sudo mkdir -p /data/web_static/shared',
+    path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
+file { '/data/web_static/releases/test/index.html':
+    content => "Test to ensure changes works",
+}
+exec { 'symbolic link':
+    command => 'sudo ln -sf /data/web_static/releases/test/ /data/web_static/current',
+    path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
+exec { 'change ownership':
+    command => 'sudo chown -hR ubuntu:ubuntu /data/',
+    path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
+exec { 'place content':
+    command => "sudo sed -i '35a\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default",
+    path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
+exec { 'restart nginx':
+    command => 'sudo /etc/init.d/nginx restart',
+    path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
