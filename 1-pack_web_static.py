f54ab66236e1cef9c8695c21c4c5ff6452fbf812
OmobaVII <omoba.career@gmail.com>
2023-07-06 08:27:16 +0100

    Compress before sending

---
+++ 1-pack_web_static.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+A Fabric Script that generates a .tgz archive
+from the contents of the web_static folder
+using a function do_pack
+"""
+from fabric.api import local
+from time import strftime
+
+
+def do_pack():
+    """generates a .tgx archive from web_static"""
+    current_time = strftime("%Y%M%d%H%M%S")
+    try:
+        local("mkdir versions")
+        myFile = f"versions/web_static_{current_time}.tgz"
+        local("tar -cvzf {} web_static/".format(myFile))
+        return myFile
+    except Exception as e:
+        return None
+
