e75a26604351958ff6361985490855fd5f6be43f
OmobaVII <omoba.career@gmail.com>
2023-07-06 08:11:18 +0000

    Compress before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,18 +5,16 @@ from the contents of the web_static folder
 using a function do_pack
 """
 from fabric.api import local
-from time import strftime
-from os.path import isdir
+from datetime import datetime
 
 
 def do_pack():
-	"""generates a .tgx archive from web_static"""
-	try:
-		current_time = strftime("%Y%M%d%H%M%S")
-		if isdir("versions") is False:
-        		local("mkdir versions")
-		myFile = "versions/web_static_{}.tgz".format(current_time)
-		local("tar -cvzf {} web_static/".format(myFile))
-		return myFile
-	except Exception as e:
-		return None
+    """generates a .tgx archive from web_static"""
+    current_time = datetime.now().strftime("%Y%M%d%H%M%S")
+    try:
+        local("mkdir -p versions")
+        myFile = "versions/web_static_{}.tgz".format(current_time)
+        local("tar -cvzf {} web_static/".format(myFile))
+        return myFile
+    except Exception as e:
+        return None
