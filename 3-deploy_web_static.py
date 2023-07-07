251207ba2bcafb1cb9038f55228d913f254cf474
OmobaVII <omoba.career@gmail.com>
2023-07-07 11:27:57 -0700

    Correction:

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -14,9 +14,8 @@ date = datetime.now()
 
 def do_pack():
     """generates a .tgx archive from web_static"""
-    path = "versions/web_static_{}{}{}{}{}{}.tgz".format(
-            date.year, date.month, date.day, date.hour,
-            date.minute, date.second)
+    path = "versions/web_static_{}.tgz".format(
+            date.strftime("%Y%m%d%H%M%S"))
     local("mkdir -p versions")
     print("Packing web_static to {}".format(path))
     pack = local("tar -cvzf " + path + " ./web_static")
@@ -30,20 +29,19 @@ def do_pack():
 def do_deploy(archive_path):
     """deploy web_static using fabric"""
     if not exists(archive_path):
-        flag = False
+        return False
     try:
-        put(archive_path, "/tmp/")
-        filename_ext = archive_path.split("/")[-1]
-        filename = filename_ext.split(".")[0]
+        file_name = archive_path.split("/")[-1]
+        no_ext = file_name.split(".")[0]
         path = "/data/web_static/releases/"
-        full_path = path + filename
-        run('mkdir -p {}/'.format(full_path))
-        run('tar -xzf /tmp/{} -C {}/'.format(filename_ext, full_path))
-        run('rm -rf /tmp/{}'.format(filename_ext))
-        run('mv {}/web_static/* {}/'.format(full_path, full_path))
-        run('rm -rf {}/web_static'.format(full_path))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}/ /data/web_static/current'.format(full_path))
+        put(archive_path, '/tmp/')
+        run('sudo mkdir -p {}{}/'.format(path, no_ext))
+        run('sudo tar -xzf /tmp/{} -C {}{}/'.format(file_name, path, no_ext))
+        run('sudo rm /tmp/{}'.format(file_name))
+        run('sudo mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('sudo rm -rf {}{}/web_static'.format(path, no_ext))
+        run('sudo rm -rf /data/web_static/current')
+        run('sudo ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         print("New version deployed!")
         flag = True
     except Exception as e:
