4b804895dce020741f2621b5744da7f753186080
OmobaVII <omoba.career@gmail.com>
2023-07-07 01:56:53 -0700

    Comprss before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,16 +10,16 @@ from datetime import datetime
 from os.path import isdir
 from os import stat
 
+date = datetime.now().strftime("%Y%m%d%H%M%S")
 
 def do_pack():
     """generates a .tgx archive from web_static"""
     try:
-        date = datetime.now().strftime("%Y%m%d%H%M%S")
         if not isdir("versions"):
             local("mkdir -p versions")
         path = "versions/web_static_{}.tgz".format(date)
         print("Packing web_static to {}".format(path))
-        local("tar -cvzf {} web_static".format(path))
+        local("tar -cvzf {} ./web_static".format(path))
         file_size = stat(path).st_size
         print("web_static packed: {} -> {} Bytes".format(path, file_size))
     except Exception:
