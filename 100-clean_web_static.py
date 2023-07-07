8466730bf382e2713b8dd2503877ad48105eea59
OmobaVII <omoba.career@gmail.com>
2023-07-07 03:18:49 -0700

    Clean UP

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -12,21 +12,17 @@ env.hosts = ['18.235.234.111', '100.25.181.230']
 
 def do_clean(number=0):
     """deletes older versions of data from web server"""
-    all_archive = os.listdir('versions/')
-    all_archive.sort(reverse=True)
-    begin = int(number)
-    if not begin:
-        begin = begin + 1
-    if begin < len(all_archive):
-        all_archive = all_archive[begin:]
+    if int(number) == 0:
+        flag = 1
     else:
-        all_archive = []
-    for archive in all_archive:
-        os.unlink('versions/{}'.format(archive))
-    cmd = [
-            "rm -rf $(",
-            "find //data/web_static/releases/ -maxdepth 1 -type d -iregex",
-            " '/data/web_static/releases/web_static_.*'",
-            " | sort -r | tr '\\n' ' ' | cut -d ' ' -f{}-)".format(start + 1)
-    ]
-    run("".join(cmd))
+        flag = int(number)
+    files = [fil for fil in os.listdir('./versions')]
+    files.sort(reverse=True)
+    for fil in files[flag:]:
+        local("rm -f versions/{}".format(fil))
+    origin = "/data/web_static/releases/"
+    with cd(origin):
+        tgz = run("ls -tr | grep -E '^web_static_([0-9]{6,}){1}$'").split()
+        tgz.sort(reverse=True)
+        for di in tgz[flag:]:
+            run("rm -rf {}{}".format(remote, di))
