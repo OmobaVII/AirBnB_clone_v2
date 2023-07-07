84e00cbbe4578683edcf6d60e9009a6cf7d78a76
OmobaVII <omoba.career@gmail.com>
2023-07-07 05:12:05 -0700

    Clean up

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -11,16 +11,16 @@ env.hosts = ['18.235.234.111', '100.25.181.230']
 
 def do_clean(number=0):
     """deletes older versions of data from web server"""
-    flag = 1 if int(number) == 0 else int(number)
-    files = [fil for fil in os.listdir('./versions')]
+    n = 1 if int(number) == 0 else int(number)
+    files = [f for f in os.listdir('./versions')]
     files.sort(reverse=True)
-    for fil in files[flag:]:
-        local("rm -f versions/{}".format(fil))
+    for f in files[n:]:
+        local("rm -f versions/{}".format(f))
     remote = "/data/web_static/releases/"
     with cd(remote):
         tgz = run(
             "ls -tr | grep -E '^web_static_([0-9]{6,}){1}$'"
         ).split()
         tgz.sort(reverse=True)
-        for d in tgz[flag:]:
+        for d in tgz[n:]:
             run("rm -rf {}{}".format(remote, d))
