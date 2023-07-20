07eb3b644e3059063c843dddae5b2f207e373c46
OmobaVII <omoba.career@gmail.com>
2023-07-20 08:21:53 -0700

    C is fun!

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Starts Flask web application
+Web app listens on 0.0.0.0 port 5000
+/hbnb page include
+/c/<text> page included
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """so the route must display Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """so the route must display HBNB"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def custom_text(text):
+    """so the route must display c + text"""
+    return "C {}".format(text.replace("_", " "))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
