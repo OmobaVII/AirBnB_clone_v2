801928f74e106bf9e35592f13404e5c3c4b79e1f
OmobaVII <omoba.career@gmail.com>
2023-07-23 09:05:36 -0700

    Try

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -10,59 +10,9 @@ Web app listens on 0.0.0.0 port 5000
 /number_odd_or_even/<n> page included
 """
 from models import storage
-from models.state import State
 from models import *
 from flask import Flask, render_template
 app = Flask(__name__)
-
-
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """so the route must display Hello HBNB!"""
-    return "Hello HBNB!"
-
-
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """so the route must display HBNB"""
-    return "HBNB"
-
-
-@app.route('/c/<text>', strict_slashes=False)
-def custom_text(text):
-    """so the route must display c + text"""
-    return "C {}".format(text.replace("_", " "))
-
-
-@app.route('/python', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def python_text(text="is cool"):
-    """so the route must display python + text"""
-    return "Python {}".format(text.replace("_", " "))
-
-
-@app.route('/number/<int:n>', strict_slashes=False)
-def number(n):
-    """so the route must display n is a number"""
-    return "{:d} is a number".format(n)
-
-
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template(n):
-    """so the route must display the number template"""
-    if isinstance(n, int):
-        return render_template('5-number.html', number=n)
-
-
-@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
-def number_odd_or_even(n):
-    """so the route must display odd or even template"""
-    if isinstance(n, int):
-        if n % 2 == 0:
-            flag = "even"
-        else:
-            flag = "odd"
-        return render_template('6-number_odd_or_even.html', n=n, flag=flag)
 
 
 @app.teardown_appcontext
@@ -71,20 +21,11 @@ def teardown_db(self):
     storage.close()
 
 
-@app.route('/states_list')
-def states_list():
-    """displays a HTML page with the list of all
-    states objects"""
-    sorted_states = [s for s in storage.all("State").values()]
-
-    return render_template('7-states_list.html', states=sorted_states)
-
-
-@app.route('/cities_by_states')
+@app.route('/cities_by_states', strict_slashes=False)
 def cities_list():
     """displays a HTML page with the list of cities and the states
     they fall under"""
-    sorted_states = storage.all(State).values()
+    sorted_states = storage.all("State").values()
     return render_template('8-cities_by_states.html', states=sorted_states)
 
 
